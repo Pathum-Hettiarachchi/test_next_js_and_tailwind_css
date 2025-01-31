e6548a89ac1c81027abe4abc148a27483463f4d8
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-02-01 02:17:37 +1300

    Updated paragraph

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='flex justify-center items-center w-full h-[100vh] relative'>
+    <div className='flex justify-center items-center w-full h-[100vh] relative flex-col'>
       <h1 className='text-3xl'>Home Page</h1>
+      <p className='w-[600px] mt-5 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ab iste sequi aliquid illo! 
+        Commodi molestias temporibus ab sunt sapiente eum perferendis earum hic quos soluta magnam, 
+        autem iure optio.</p>
       </div>
   )
 }
