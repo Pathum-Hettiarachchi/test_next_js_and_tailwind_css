26b6990794cb53815958b7b07503136ed6832995
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-02-01 01:14:45 +1300

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>Home page</div>
+    <div className='flex justify-center items-center w-full h-[100vh] relative'>
+      <h1 className='text-3xl'>Home Page</h1>
+      </div>
   )
 }
