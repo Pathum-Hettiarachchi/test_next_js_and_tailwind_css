6dd994f78f71384d408b2c852d637c052fd6f9f7
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-02-01 17:53:33 +1300

    button updated

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,11 @@ export default function page() {
       <h1 className='text-3xl'>Home Page</h1>
       <p className='w-[600px] mt-5 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ab iste sequi aliquid illo! 
         Commodi molestias temporibus ab sunt sapiente eum perferendis earum hic quos soluta magnam, 
-        autem iure optio.</p>
+        autem iure option.</p>
+
+        <div>
+          <button className='bg-blue-500 px-8 py-2 mt-5 rounded-full text-red-50'>Submit</button>
+          </div>
         
       </div>
   )
