24bf3ce01f54fa0d4a9ce360a86794af8795cdb4
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-02-03 05:23:07 +1300

    Navigation Updated

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,12 @@ import React from 'react'
 export default function page() {
   return (
     <div className='flex justify-center items-center w-ful bg-blue-200 h-[100vh] relative flex-col'>
+      <div className="navBar">
+        <li><a href="">Service</a></li>
+        <li><a href="">Project</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Lets Talk</a></li>
+      </div>
       <h1 className='text-3xl'>Home Page</h1>
       <p className='w-[600px] mt-5 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ab iste sequi aliquid illo! 
         Commodi molestias temporibus ab sunt sapiente eum perferendis earum hic quos soluta magnam, 
