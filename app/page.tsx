64713699f40af8313b1725bdb3b30ad7e66f3e6a
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-02-05 17:27:09 +1300

    Pages Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 
+
 export default function page() {
   return (
     <div className='flex justify-center items-center w-ful bg-blue-200 h-[100vh] relative flex-col'>
       <div className="navBar absolute top-0 left-0 flex w-full py-3 items-center justify-center">
         <div className="nav-items flex bg-white bg-opacity-80 py-3 px-10 rounded-full list-none font-semibold text-gray-700">
-        <li className='mr-8'><a href="">Service</a></li>
-        <li className='mr-8'><a href="">Project</a></li>
-        <li className='mr-8'><a href="">About</a></li>
+        <li className='mr-8'><a href="/service">Service</a></li>
+        <li className='mr-8'><a href="/project">Project</a></li>
+        <li className='mr-8'><a href="/about">About</a></li>
         <li className='mr-8'><a href="">Lets Talk</a></li>
         </div>
       </div>
-      <h1 className='text-3xl'>Home Page</h1>
+      <h1 className='text-[64px]'>Home Page</h1>
       <p className='w-[600px] mt-5 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ab iste sequi aliquid illo! 
         Commodi molestias temporibus ab sunt sapiente eum perferendis earum hic quos soluta magnam, 
         autem iure option.</p>
