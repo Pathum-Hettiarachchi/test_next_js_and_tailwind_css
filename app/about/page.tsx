bc08216276c0b7c7014332be945767f1ea0c4fc6
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-02-06 17:00:17 +1300

    Page bg and text Updated

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='flex justify-center items-center bg-blue-200 w-full h-[100vh] text-3xl'>
+    <h1>About Page</h1>
+    </div>
   )
 }
