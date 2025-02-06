feace3b2d6f95cbc91dbbee031b0a0c46610a6fa
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-02-06 17:00:24 +1300

    Page bg and text Updated

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>Project</div>
+    <div className='flex justify-center items-center bg-blue-200 w-full h-[100vh] text-3xl'>
+    <h1>Projec Page</h1>
+    </div>
   )
 }
