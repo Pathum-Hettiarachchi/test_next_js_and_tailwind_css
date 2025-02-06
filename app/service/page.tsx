b270f2304ff9a409c3fa60854b82e781a3636f93
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-02-06 17:00:12 +1300

    Page bg and text Updated

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function service() {
   return (
-    <div>service</div>
+    <div className='flex justify-center items-center bg-gray-200 w-full h-[100vh] text-3xl'>
+      <h1>Service Page</h1>
+      </div>
   )
 }
