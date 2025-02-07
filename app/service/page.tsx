774d29c5470c575eeebf5577e30fac14ea88e077
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-02-07 15:34:58 +1300

    page body text updated

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function service() {
   return (
-    <div className='flex justify-center items-center bg-gray-200 w-full h-[100vh] text-3xl'>
-      <h1>Service Page</h1>
+    <div className='flex flex-col justify-center items-center bg-gray-200 w-full h-[100vh]'>
+      <h1 className='text-3xl'>Service Page</h1>
+      <p className='w-[600px] mt-5 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ab iste sequi aliquid illo! 
+        Commodi molestias temporibus ab sunt sapiente eum perferendis earum hic quos soluta magnam, 
+        autem iure option.</p>
       </div>
   )
 }
