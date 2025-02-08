44357db0451e3bf8bcaf2d1d7de9e828392263cd
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-02-08 18:41:55 +1300

    background changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='flex justify-center items-center w-ful bg-blue-200 h-[100vh] relative flex-col'>
+    <div className='flex justify-center items-center w-ful bg-background h-[100vh] relative flex-col'>
       <div className="navBar absolute top-0 left-0 flex w-full py-3 items-center justify-center">
         <div className="nav-items flex bg-white bg-opacity-80 py-3 px-10 rounded-full list-none font-semibold text-gray-700">
         <li className='mr-8'><a href="/service">Service</a></li>
@@ -12,8 +12,8 @@ export default function page() {
         <li className='mr-8'><a href="">Lets Talk</a></li>
         </div>
       </div>
-      <h1 className='text-[64px]'>Home Page</h1>
-      <p className='w-[600px] mt-5 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ab iste sequi aliquid illo! 
+      <h1 className='text-[64px] text-font-color'>Home Page</h1>
+      <p className='w-[600px] mt-5 text-center text-bodytext'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ab iste sequi aliquid illo! 
         Commodi molestias temporibus ab sunt sapiente eum perferendis earum hic quos soluta magnam, 
         autem iure option.</p>
 
