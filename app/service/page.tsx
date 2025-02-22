439ac8b3e962039fcb1ffc44e901e897eadeafc8
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-02-22 20:17:23 +1300

    background color updated

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function service() {
   return (
-    <div className='flex flex-col justify-center items-center bg-gray-200 w-full h-[100vh]'>
+    <div className='flex flex-col justify-center items-center  bg-background w-full h-[100vh]'>
       <h1 className='text-3xl'>Service Page</h1>
       <p className='w-[600px] mt-5 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ab iste sequi aliquid illo! 
         Commodi molestias temporibus ab sunt sapiente eum perferendis earum hic quos soluta magnam, 
