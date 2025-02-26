2090fa6f1d6aaee9e55a31169a7dfc829f022a1c
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-02-26 22:02:53 +1300

    section upadated

--- app/page.tsx
+++ app/page.tsx
@@ -68,6 +68,20 @@ export default function page() {
 <div className="section-title bg-background">
     <h1 className='text-[86px] text-fontcolor p-[20px] text-center'>About Me</h1>
   </div>
+  <section className='w-[96%] bg-cardbg2 flex text-fontcolor h-[120px] rounded-2xl gap-4'>
+    <div className="items">
+      <h1>20+</h1>
+      <p>Projects</p>
+    </div>
+    <div className="items">
+      <h1>15+</h1>
+      <p>Clients</p>
+    </div>
+    <div className="items">
+      <h1>4+</h1>
+      <p>Years</p>
+    </div>
+  </section>
   <section className='flex justify-center items-center w-full h-auto'>
     <SignificantOthers />
     </section>
