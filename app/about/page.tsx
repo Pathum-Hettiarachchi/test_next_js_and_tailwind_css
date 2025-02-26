575e75e25c9bfee8a1b1f6e0ad47e04c582dbc33
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-02-26 21:52:39 +1300

    page updated

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
   return (
     <div className='flex flex-col justify-center items-center bg-background w-full h-[100vh]'>
-    <h1 className='text-3xl'>About Page</h1>
+    <h1 className='text-[80px]'>About Page</h1>
     <p className='w-[600px] mt-5 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ab iste sequi aliquid illo! 
         Commodi molestias temporibus ab sunt sapiente eum perferendis earum hic quos soluta magnam, 
         autem iure option.</p>
