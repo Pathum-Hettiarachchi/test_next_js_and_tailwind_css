c2360b20d7eece3fd47b6535ff8d0cfebfc2baf7
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-03-01 20:17:13 +1300

    add nav bar

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -3,8 +3,7 @@ import React from 'react'
 export default function service() {
   return (
     <div className='flex flex-col justify-center items-center  bg-background w-full h-[100vh] p-10'>
-
-<div className="navBar absolute top-0 left-0 flex w-full py-3  items-center justify-center">
+    <div className="navBar absolute top-0 left-0 flex w-full py-3  items-center justify-center">
         <div className="nav-items flex bg-white bg-opacity-80 py-5 px-10 rounded-2xl list-none font-semibold text-gray-700">
         <li className='mr-8'><a href="/service">Service</a></li>
         <li className='mr-8'><a href="/project">Project</a></li>
