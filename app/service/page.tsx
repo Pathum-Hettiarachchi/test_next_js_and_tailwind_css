2c3a2b333e7fdff0f5804753c0eceac3a11feb2f
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-03-03 21:48:01 +1300

    add home

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -6,6 +6,7 @@ export default function service() {
     <div className='flex flex-col justify-center items-center  bg-background w-full h-[100vh] p-10'>
     <div className="navBar absolute top-0 left-0 flex w-full py-3  items-center justify-center">
         <div className="nav-items flex bg-white bg-opacity-80 py-5 px-10 rounded-2xl list-none font-semibold text-gray-700">
+        <li className='mr-8'><a href="/.">Home</a></li>
         <li className='mr-8'><a href="/service">Service</a></li>
         <li className='mr-8'><a href="/project">Project</a></li>
         <li className='mr-8'><a href="/about">About</a></li>
@@ -19,9 +20,9 @@ export default function service() {
         Commodi molestias temporibus ab sunt sapiente eum perferendis earum hic quos soluta magnam, 
         autem iure option.</p>
 
-            <section className='flex justify-center items-center w-full h-auto absolute bottom-0'>
-            <Footer />
-            </section>
+      <section className='flex justify-center items-center w-full h-auto absolute bottom-0'>
+      <Footer />
+      </section>
       </div>
   )
 }
