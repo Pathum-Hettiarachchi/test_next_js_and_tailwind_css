65ccbd006f32867c5f41d8082de5d1f123f7346a
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-03-03 21:58:08 +1300

    add home page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,6 +6,7 @@ export default function About() {
     <div className='flex flex-col justify-center items-center bg-background w-full h-[100vh]'>
           <div className="navBar absolute top-0 left-0 flex w-full py-3  items-center justify-center">
         <div className="nav-items flex bg-white bg-opacity-80 py-5 px-10 rounded-2xl list-none font-semibold text-gray-700">
+        <li className='mr-8'><a href="/.">Home</a></li>
         <li className='mr-8'><a href="/service">Service</a></li>
         <li className='mr-8'><a href="/project">Project</a></li>
         <li className='mr-8'><a href="/about">About</a></li>
