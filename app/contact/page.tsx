532eb57b224bf086caeafcf9af7c7b5103c65d83
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-03-11 06:29:47 +1300

    contact page updated

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Footer from '../Components/Footer'
+import ContactSection from '../Components/contactsection'
 
 export default function page() {
   return (
@@ -13,11 +14,7 @@ export default function page() {
         <li className='mr-8'><a href="">Lets Talk</a></li>
         </div>
       </div>
-    <h1 className='text-[80px] text-fontcolor'>Contact Page</h1>
-    <p className='w-[600px] mt-5 text-center text-fontcolor'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ab iste sequi aliquid illo! 
-        Commodi molestias temporibus ab sunt sapiente eum perferendis earum hic quos soluta magnam, 
-        autem iure option.</p>
-
+      <ContactSection />
      <section className='flex justify-center items-center w-full h-auto absolute bottom-0'>
     <Footer />
     </section>
