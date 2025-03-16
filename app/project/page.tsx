334a01cc00c3ef61d017034a63b2aef5721404ee
Pathum-Hettiarachchi <fiverrpasinduu@gmail.com>
2025-03-17 03:26:25 +1300

    page updated

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from '../Components/Footer'
+import LatestProjects from '../Components/recentProject'
+
 
 export default function page() {
   return (
-    <div className='flex flex-col justify-center items-center bg-background w-full h-[100vh] '>
+    <div className='flex flex-col justify-center items-center bg-background w-full h-[auto] relative'>
 
-    <div className="navBar absolute top-0 left-0 flex w-full py-3  items-center justify-center">
+    <div className="navBar absolute top-0 left-0 flex w-full py-3   justify-center">
         <div className="nav-items flex bg-white bg-opacity-80 py-5 px-10 rounded-2xl list-none font-semibold text-gray-700">
         <li className='mr-8'><a href="/service">Service</a></li>
         <li className='mr-8'><a href="/project">Project</a></li>
@@ -17,8 +19,8 @@ export default function page() {
     <p className='w-[600px] mt-5 text-center text-fontcolor'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ab iste sequi aliquid illo! 
         Commodi molestias temporibus ab sunt sapiente eum perferendis earum hic quos soluta magnam, 
         autem iure option.</p>
-
-     <section className='flex justify-center items-center w-full h-auto absolute bottom-0'>
+        <LatestProjects/>
+    <section className='flex justify-center items-center w-full h-[auto] absolute bottom-0'>
     <Footer />
     </section>
     </div>
